aa6c3f55aec351eac073f72a5279a03d4fb914d3
EmmanuelOuzan <Emmanuel.ouzan@gmail.com>
2021-12-30 19:12:19 +0200

    Final_TO_me

--- DAL/db.js
+++ DAL/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 const
-    userName = 'EmmanuelOuzan',
-    password = 'Manu6262',
+    userName = 'Username',
+    password = 'Password',
     connectionString = `mongodb+srv://${userName}:${password}@emmanuelmongocluster.a8zm2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 console.log('connecting ..')
