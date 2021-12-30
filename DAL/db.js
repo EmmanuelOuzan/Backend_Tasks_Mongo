cf15f78f83e8923112a4f3cd7b27df118d55d1a8
EmmanuelOuzan <Emmanuel.ouzan@gmail.com>
2021-12-30 19:07:10 +0200

    Try

--- DAL/db.js
+++ DAL/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 const
-    userName = 'workin',
-    password = 'Workin1121',
-    connectionString = `mongodb+srv://${userName}:${password}@cluster0.ho2ly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    userName = 'EmmanuelOuzan',
+    password = 'Manu6262',
+    connectionString = `mongodb+srv://${userName}:${password}@emmanuelmongocluster.a8zm2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 console.log('connecting ..')
 mongoose.connect(connectionString)
